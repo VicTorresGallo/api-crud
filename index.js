52281187571dca335a7b7a1eef75a0622c1908d7
VicTorresGallo <vicentg1211@gmail.com>
2024-02-05 16:21:58 +0100

    Nodemon y Morgan

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 'use strict'
-const port = process.env.PORT || 8888;
+
+const port = process.env.PORT || 3000;
+
 const express = require('express');
+const logger = require('morgan');
+
 const app = express();
-app.get('/hola/:unNombre', (req, res) => {
-res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde Express!` });
-});
-app.listen(port, () => {
- console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
-}); 
+    //middlewares
+app.use(logger('dev'));
+    //añadir el método get para obtener el parametro
+    app.get('/hola/:unNombre', (req, res) => {
+        res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde JSON!` });
+    });
+
+    //listener
+    app.listen(port, () => {
+        console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
+    }); 
