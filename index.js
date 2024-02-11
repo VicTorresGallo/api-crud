08c1892dff4774ed98c84e6eb23f8d2becc07365
VicTorresGallo <vicentg1211@gmail.com>
2024-02-11 20:20:43 +0100

    api rest crud sin BD

--- index.js
+++ index.js
@@ -7,13 +7,51 @@ const logger = require('morgan');
 
 const app = express();
     //middlewares
-app.use(logger('dev'));
+    app.use(logger('dev'));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
     //añadir el método get para obtener el parametro
-    app.get('/hola/:unNombre', (req, res) => {
-        res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde JSON!` });
+
+    app.get('/api/product', (req, res) => {
+        res.status(200).send({ productos: []});
     });
+    app.get('/api/product/:productID', (req, res) => {
+        const productID = req.params.productID;
+        res.status(200).send({ product: productID});
+    });
+    app.post('/api/product', (req, res) => {
+        const queProducto = req.body;
+        console.log(queProducto);
+        res.status(200).send({ 
+            mensaje:'producto creado',
+            producto:queProducto
+        });
+    });
+
+    app.put('/api/product/:productID',(req,res) => {
+        const queProducto = req.body;
+        const productID = req.params.productID;
+
+        res.status(200).send({
+            mensaje: `Se ha modificado el producto ${productID}`,
+            producto: queProducto
+        });
+    });
+
+    app.delete('/api/product/:productID', (req, res) => {
+        const productID = req.params.productID;
+        res.status(200);
+        res.send( { mensaje: `Se ha eliminado el producto ${productID}` } );
+        });
+
+    /*app.delete('/api/product/:productID', (req,res) => {
+        const productID = req.params.productID;
+        req.status(200).send({
+            mensaje:'se ha eliminado el producto ${productID}'
+        });
+    });*/
 
     //listener
     app.listen(port, () => {
-        console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
+        console.log(`API REST ejecutándose en http://localhost:${port}/api/product`);
     }); 
