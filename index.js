96dcccdd83951ad37e38d60be01763d531da19fe
VicTorresGallo <vicentg1211@gmail.com>
2024-02-25 11:54:18 +0100

    prueba API REST CRUD con MongoDB: https y helmet (guia 5)

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 'use strict'
+
 // inportaciones
+
 const config = require('./config');
 const express = require('express');
 const logger = require('morgan');
 const mongojs = require('mongojs');
 const cors = require('cors');
+const helmet = require('helmet');
+var fs = require('fs');
+var https = require('https');
+
 // Declaraciones
+
 const port = config.PORT;
 const urlDB = config.DB;
 const accessToken = config.TOKEN;
 const app = express();
 const db = mongojs(urlDB); // Enlazamos con la DB
 const id = mongojs.ObjectID; // Función para convertir un id textual en un objectID
+
+
 // Declaraciones para CORS
+
 var allowCrossTokenOrigin = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // Permiso a cualquier URL. Mejor acotar
-return next();
+    return next();
 };
 var allowCrossTokenMethods = (req, res, next) => {
     res.header("Access-Control-Allow-Methods", "*"); // Mejor acotar (GET,PUT,POST,DELETE)
-return next();
+    return next();
 };
 var allowCrossTokenHeaders = (req, res, next) => {
     res.header("Access-Control-Allow-Headers", "*"); // Mejor acotar (Content-type)
-return next();
+    return next();
 };
 // middleware
 var auth = (req, res, next) => { // declaramos la función auth
     if ( !req.headers.token ) { // si no se envía el token...
         res.status(401).json({ result: 'NO', msg: "Envía un código válido en la cabecera 'token'"});
-return;
+        return;
     };
     const queToken = req.headers.token; // recogemos el token de la cabecera llamada “token”
     if ( queToken === accessToken ) { // si coincide con nuestro password...
@@ -38,7 +48,9 @@ return;
         res.status(401).json({ result: 'NO', msg: "No autorizado" });
     };
 };
+
 // middlewares
+
 app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
 app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(express.json()); // parse application/json
@@ -46,23 +58,29 @@ app.use(cors()); // activamos CORS
 app.use(allowCrossTokenOrigin); // configuramos origen permitido para CORS
 app.use(allowCrossTokenMethods); // configuramos métodos permitidos para CORS
 app.use(allowCrossTokenHeaders); // configuramos cabeceras permitidas para CORS
+app.use(helmet());// activamos helmet
+
 // routes
+
 app.param("coleccion", (req, res, next, coleccion) => {
     req.collection = db.collection(coleccion);
     return next();
 });
+
 app.get('/api', (req, res, next) => {
     db.getCollectionNames((err, colecciones) => {
         if (err) return next(err);
         res.json(colecciones);
     });
 });
+
 app.get('/api/:coleccion', (req, res, next) => {
     req.collection.find((err, coleccion) => {
         if (err) return next(err);
             res.json(coleccion);
     });
 });
+
 app.get('/api/:coleccion/:id', (req, res, next) => {
     const elementoId = req.params.id;
     req.collection.findOne({ _id: id(elementoId) }, (err, elementoRecuperado) => {
@@ -70,6 +88,7 @@ app.get('/api/:coleccion/:id', (req, res, next) => {
             res.json(elementoRecuperado);
     });
 });
+
 app.post('/api/:coleccion', auth, (req, res, next) => {
     const nuevoElemento = req.body;
     req.collection.save(nuevoElemento, (err, coleccionGuardada) => {
@@ -77,6 +96,7 @@ app.post('/api/:coleccion', auth, (req, res, next) => {
             res.json(coleccionGuardada);
     });
 });
+
 app.put('/api/:coleccion/:id', auth, (req, res, next) => {
     const elementoId = req.params.id;
     const nuevosRegistros = req.body;
@@ -88,6 +108,7 @@ app.put('/api/:coleccion/:id', auth, (req, res, next) => {
         res.json(result);
     });
 });
+
 app.delete('/api/:coleccion/:id', auth, (req, res, next) => {
     const elementoId = req.params.id;
     req.collection.remove({ _id: id(elementoId) }, (err, resultado) => {
@@ -95,7 +116,12 @@ app.delete('/api/:coleccion/:id', auth, (req, res, next) => {
         res.json(resultado);
     });
 });
+
 // Iniciamos la aplicación
-app.listen(port, () => {
+
+https.createServer({
+        cert: fs.readFileSync('./cert/cert.pem'),
+        key: fs.readFileSync('./cert/key.pem')
+    },app).listen(port, () => {
     console.log(`API RESTful CRUD ejecutándose en https://localhost:${port}/api/{colecciones}/{id}`);
 }); 
